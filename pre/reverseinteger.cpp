bc66a844b752c094b565cf32b679f00863a7cbc4
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-22 23:41:31 +0530

    Update reverseinteger.cpp

--- pre/reverseinteger.cpp
+++ pre/reverseinteger.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+  
+	int n;
+  //input
+	cin>>n;
+  
+	int r,ans=0;
+	while(n>0){
+		r=n%10;
+		if((ans>INT_MAX/10) || (ans<INT_MIN/10)){
+			return 0;
+		}
+		ans=ans*10 + r;
+		n=n/10;
+	}
+	cout<<ans<<endl;
+}
