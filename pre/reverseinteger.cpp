ca504f178c1d834406dcb6deaf0f69e99d688e47
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-22 23:50:38 +0530

    Update reverseinteger.cpp

--- pre/reverseinteger.cpp
+++ pre/reverseinteger.cpp
@@ -4,17 +4,20 @@ using namespace std;
 int main(){
   
 	int n;
-  //input
+  	//input
 	cin>>n;
   
 	int r,ans=0;
+	//r- remainder
+	//ans- reverse
 	while(n>0){
 		r=n%10;
 		if((ans>INT_MAX/10) || (ans<INT_MIN/10)){
 			return 0;
+			//just in case your number (i.e. reverse*10<max or min) exceeds the upper bound or lower bound which is yk -2^(31) to 2^(31)-1 
 		}
 		ans=ans*10 + r;
 		n=n/10;
 	}
-	cout<<ans<<endl;
+	cout<<"reverse of number is : "<<ans<<endl;
 }
