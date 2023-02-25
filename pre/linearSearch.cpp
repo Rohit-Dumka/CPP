2c57b1c95ba99edace6a74d703ad9ea9fe384121
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-25 23:22:39 +0530

    Create linearSearch.cpp

---
+++ pre/linearSearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[],int size,int key){
+	for(int i=0; i<size;i++){
+		if(arr[i]==key){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(){
+//	static array
+	int arr[10]={5,7,-2,10,22,-2,0,5,22,1};
+	
+	cout<<"enter the key"<<endl;
+	int key;
+	cin>>key;
+
+	bool found=search(arr,10,key);
+
+	if(found)
+	cout<<"key is present"<<endl;
+	else
+	cout<<"key isn't present"<<endl;
+
+}
