295c4f6e43784f789add444eec71c8265875401e
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-25 23:26:16 +0530

    Update linearSearch.cpp

--- pre/linearSearch.cpp
+++ pre/linearSearch.cpp
@@ -11,21 +11,6 @@ bool search(int arr[],int size,int key){
 }
 
 int main(){
-//	static array
-	int arr[10]={5,7,-2,10,22,-2,0,5,22,1};
-#include<iostream>
-using namespace std;
-
-bool search(int arr[],int size,int key){
-	for(int i=0; i<size;i++){
-		if(arr[i]==key){
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int main(){
 	//	static array
 	int arr[10]={5,7,-2,10,22,-2,0,5,22,1};
 	
@@ -37,19 +22,8 @@ int main(){
 	//	for(i=0;i<size;i++)
 	//	cin>>arr[i];
 
-	cout<<"enter the key"<<endl;
-	int key;
-	cin>>key;
 
-	bool found=search(arr,10,key);
-
-	if(found)
-	cout<<"key is present"<<endl;
-	else
-	cout<<"key isn't present"<<endl;
-
-}
-	
+	//check whether key(input element) is present or not
 	cout<<"enter the key"<<endl;
 	int key;
 	cin>>key;
