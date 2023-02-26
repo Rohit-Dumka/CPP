4179d15de4e35966ddc7074513c753efc0e5ee56
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-26 22:55:09 +0530

    Update removeDuplicates.cpp

--- pre/removeDuplicates.cpp
+++ pre/removeDuplicates.cpp
@@ -16,7 +16,8 @@ using namespace std;
     int main(){
     	int nums[13]={ 1,2,3,3,5,8,8,8,8,11,15,17,17 };
     	int n=removeDuplicates(nums);
-    	
+	    
+    	//printing final array with no duplicate values--
     	for(int i=0;i<n;i++)
     	cout<<" "<<nums[i];
 	}
