32a3bf3ab5a7c5b5a17157c75b4f5e84b60997af
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-26 22:44:39 +0530

    Update removeDuplicates.cpp

--- pre/removeDuplicates.cpp
+++ pre/removeDuplicates.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+    int removeDuplicates(int nums[]) {
+    int j=1;
 
+	for(int i=1; i<13-1; i++)        
+		if(nums[i]!=nums[i-1])
+            nums[j++] = nums[i];
+
+	return j;
+    }
+    int main(){
+    	int nums[13]={ 1,2,3,3,5,8,8,8,8,11,15,17,17 };
+    	int n=removeDuplicates(nums);
+    	
+    	for(int i=0;i<n;i++)
+    	cout<<" "<<nums[i];
+	}
