d4bfd435d898295141826e388bf3478eafa04eec
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-02-27 23:30:23 +0530

    Update arrayReverse.cpp

--- pre/arrayReverse.cpp
+++ pre/arrayReverse.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void reverse(int arr[],int n){
 	int start=0;
 	int end =n-1;
-
+//cpp has direct function to swap elements ,we can use it to save our time :)
 	while(start<=end){
 		swap(arr[start],arr[end]);
 		start++;
