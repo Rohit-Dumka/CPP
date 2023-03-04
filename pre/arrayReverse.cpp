6d40e9ea2f6122b5881a30452cec5a7cc45a5a72
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-03-04 21:54:02 +0530

    Update

--- pre/arrayReverse.cpp
+++ pre/arrayReverse.cpp
@@ -14,6 +14,7 @@ void reverse(int arr[],int n){
 
 
 void printArray(int arr[],int n){
+
 	for(int i=0;i<n;i++){
 		cout<<arr[i]<<" ";
 	}
@@ -24,13 +25,13 @@ int main(){
 	int arr[6]={1,4,0,5,-2,15};
 	int brr[5]={2,6,3,9,4};
 
-	cout<<"original arrays: "<<endl;
+	cout<<"original arrays: "<<"\n";
 	printArray(arr,6);
 	printArray(brr,5);
 	
 	reverse(arr,6);
 	reverse(brr,5);
-	cout<<"reversed arrays: "<<endl;
+	cout<<"reversed arrays: "<<"\n";
 	printArray(arr,6);
 	printArray(brr,5);
 }
