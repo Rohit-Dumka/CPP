57e9e6c1bb509e5bd493ce2ede3659b8c690caa1
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-03-11 09:57:37 +0530

    Create InterSectionAdv.cpp

---
+++ mid/InterSectionAdv.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>//to use vector
+#include<algorithm>//to perform sorting
+using namespace std;
+int main(){
+vector<int>a{1,2,3,3,4,5,1,5};
+vector<int>b{3,3,5,4,4,5,6};
+
+vector<int>ans;
+sort(a.begin(),a.end());
+sort(b.begin(),b.end());
+
+for(int i=0;i<a.size();i++){
+        if(i>1 && a[i-1]==a[i]){//for dups in a
+            continue;
+        }
+    for(int j=0;j<b.size();j++){
+        if(a[i]==b[j]){
+            ans.push_back(a[i]);
+            break;//for dups in b
+        }
+    }
+}
+for(auto x:ans){
+    cout<<x<<" ";
+}
+
+}
