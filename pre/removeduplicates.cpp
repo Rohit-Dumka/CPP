c90ce9d95cb82d20998f35499145ea92703d2119
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-07-24 04:37:32 -0700

    Update removeduplicates.cpp

--- pre/removeduplicates.cpp
+++ pre/removeduplicates.cpp
@@ -18,3 +18,24 @@ int main() {
     for(int i=0;i<j;i++)
         cout<<temp[i]<<" ";
 }
+
+//method 2--------------------------
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a[]={10,20,20,30,30,30,40};
+    int n=sizeof(a)/sizeof(int);
+    int j=1;
+    
+    for(int i=1;i<n;i++)
+    {
+        if(a[j-1]!=a[i])
+        {
+            a[j]=a[i];
+            j++;
+        }
+    }
+    for(int i=0;i<j;i++)
+        cout<<a[i]<<" ";
+}
