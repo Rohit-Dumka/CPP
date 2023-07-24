eae46f53913c3b48313b46c14db958e0c15ee7f8
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-07-24 04:35:02 -0700

    Create removeduplicates.cpp

---
+++ pre/removeduplicates.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[]={10,20,20,30,30,30,40};
+    int n=sizeof(a)/sizeof(int);
+    int temp[n];
+    temp[0]=a[0];
+    int j=1;
+    
+    for(int i=1;i<n;i++)
+    {
+        if(temp[j-1]!=a[i])
+        {
+            temp[j]=a[i];
+            j++;
+        }
+    }
+    for(int i=0;i<j;i++)
+        cout<<temp[i]<<" ";
+}
