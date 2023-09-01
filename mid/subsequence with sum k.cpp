751fcf2c6e98ceea2ae11408514b9e672ea96873
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-09-02 02:05:37 +0530

    Create subsequence with sum k.cpp

---
+++ mid/subsequence with sum k.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool print(int i,vector<int>&a,int s,int sum,int arr[],int n){
+    if(i==n){
+        if(s==sum){
+            for(int x:a)
+                cout<<x<<" ";
+            cout<<endl;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    a.push_back(arr[i]);
+    s+=arr[i];
+    
+    if(print(i+1,a,s,sum,arr,n)==true)
+        return true;
+    
+    s-=arr[i];
+    a.pop_back();
+    
+    if(print(i+1,a,s,sum,arr,n)==true)
+        return true;
+    return false;
+}
+int main() {
+   int arr[]={1,2,1};
+   int n=3;
+   int sum=2;
+   vector<int>a;
+   print(0,a,0,sum,arr,n);
+}
