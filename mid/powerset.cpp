d85dc0a81088cbdb13d718f9f7d41f693b5602b1
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-09-02 01:29:37 +0530

    Update powerset.cpp

--- mid/powerset.cpp
+++ mid/powerset.cpp
@@ -22,3 +22,30 @@ int main() {
     vector<int>a;
     print(0,a,arr,n);
 }
+
+//-------------------------------rev order-----------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+void print(int i,vector<int>&a,int arr[],int n){
+    if(i==n){
+        for(int x: a)
+            cout<<x<<" ";
+        if(a.size()==0)
+            cout<<"{}";
+        cout<<endl;
+            return;
+    }
+    print(i+1,a,arr,n);
+    
+    a.push_back(arr[i]);
+    print(i+1,a,arr,n);
+    a.pop_back();
+
+}
+int main() {
+    int arr[]={3,1,2};
+    int n=3;
+    vector<int>a;
+    print(0,a,arr,n);
+}
