fdd7338327b1eb51756621df7f0bd5025b85883c
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-09-02 01:26:36 +0530

    Create powerset.cpp

---
+++ mid/powerset.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int i,vector<int>&a,int arr[],int n){
+    if(i==n){
+        for(int x: a)
+            cout<<x<<" ";
+        if(a.size()==0)
+            cout<<"{}";
+        cout<<endl;
+            return;
+    }
+    //take or pick the particular index into the subsequence
+    a.push_back(arr[i]);
+    print(i+1,a,arr,n);
+    a.pop_back();
+    //not pick, or not take condition, this element is not added to your subsequence
+    print(i+1,a,arr,n);
+}
+int main() {
+    int arr[]={3,1,2};
+    int n=3;
+    vector<int>a;
+    print(0,a,arr,n);
+}
