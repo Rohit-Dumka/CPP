3a7f233fc6a5e3584c3a6ecce1a4ba4a1983100b
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-11-14 12:01:03 +0530

    Update first.cpp

--- stl/Stack/first.cpp
+++ stl/Stack/first.cpp
@@ -15,3 +15,55 @@ int main() {
         s.pop();
     }
 }
+
+//-------------------------------------
+
+#include <iostream>
+#include <stack>
+using namespace std;
+void printStack(stack<int>s){
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
+int main() {
+    stack <int> s;
+    s.push(1);
+    s.push(3);
+    s.push(5);
+    s.push(9);
+    printStack(s);
+}
+
+//----------------------------------------
+
+#include <iostream>
+#include <stack>
+using namespace std;
+void printStack(stack<int>s){
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+}
+int main() {
+    stack <int> s,s2;
+    // stack<stack<int>>s;
+    // stack<vector<int>>s;
+    s.push(1);
+    s.push(3);
+    s.push(5);
+    s.push(9);
+    printStack(s);
+    s2.push(2);
+    s2.push(6);
+    s2.push(12);
+    s2.push(8);
+    printStack(s2);
+    cout<<"after swap: "<<endl;
+    s.swap(s2);
+    printStack(s);
+    printStack(s2);
+}
